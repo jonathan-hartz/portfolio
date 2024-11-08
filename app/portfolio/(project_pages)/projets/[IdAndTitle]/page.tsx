63eae392bac598a8095cc5c8443e96c51d260b34
Jonathan Hartz <jonat.hartz@gmail.com>
2024-11-08 10:59:06 +0100

    Adaptation mobile et lighthouse

--- app/portfolio/(project_pages)/projets/[IdAndTitle]/page.tsx
+++ app/portfolio/(project_pages)/projets/[IdAndTitle]/page.tsx
@@ -55,14 +55,14 @@ export default async function Page({
               </div>
             </div>
           </div>
-          <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
+          <div className="mx-auto p-4 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:-ml-12 lg:-mt-12 lg:overflow-hidden lg:p-12">
             <Image
               alt=""
               sizes="48rem"
               width={0}
               height={0}
               src={`/projects/${project.imagePath}`}
-              className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
+              className="w-[48rem] rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] lg:max-w-none"
             />
           </div>
           <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
--- app/portfolio/components/animations/Transition.tsx
+++ app/portfolio/components/animations/Transition.tsx
@@ -3,18 +3,24 @@ import { motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 import { isPreviousPageBeforeCurrentPage } from "@/app/portfolio/components/navigation/MenuEntries";
 
-const getVariants = (isTheScrollBottomToTop: boolean) => {
-  return {
-    initial: {
+const getVariants = (
+  isTheScrollBottomToTop: boolean,
+  isANewNavigation: boolean,
+) => {
+  const variants = {
+    initial: {},
+    animate: {},
+    exit: {},
+  };
+  if (!isANewNavigation) {
+    variants.initial = {
       y: isTheScrollBottomToTop ? 1500 : -1500,
       opacity: 0.5,
-    },
-    animate: { y: 0, opacity: 1 },
-    exit: {
-      y: isTheScrollBottomToTop ? -1500 : 1500,
-      opacity: 0.5,
-    },
-  };
+    };
+    variants.animate = { y: 0, opacity: 1 };
+    variants.exit = { y: isTheScrollBottomToTop ? -1500 : 1500, opacity: 0.5 };
+  }
+  return variants;
 };
 
 export default function Transition({
@@ -24,10 +30,12 @@ export default function Transition({
 }) {
   const current_page_pathname = usePathname();
   let isTheScrollBottomToTop = false;
+  let isANewNavigation = true;
   let previous_page_pathname = null;
-  if (typeof window !== "undefined")
+  if (typeof window !== "undefined" && localStorage.getItem("previousPage")) {
+    isANewNavigation = false;
     previous_page_pathname = localStorage.getItem("previousPage");
-
+  }
   if (
     previous_page_pathname != null &&
     current_page_pathname != previous_page_pathname
@@ -37,7 +45,7 @@ export default function Transition({
       previous_page_pathname,
     );
   }
-  const variants = getVariants(isTheScrollBottomToTop);
+  const variants = getVariants(isTheScrollBottomToTop, isANewNavigation);
 
   return (
     <>
